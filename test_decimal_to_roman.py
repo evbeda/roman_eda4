0a5edd5bdf5035b9248c3875081621f9a23834b0
Matias Sotelo <msotelo@eventbrite.com>
2020-03-17 15:27:32 -0300

    Adding test from 2 to 20 afgain

--- test_decimal_to_roman.py
+++ test_decimal_to_roman.py
@@ -13,6 +13,79 @@ class TestDecimalToRoman(unittest.TestCase):
         result = decimal_to_roman(1)
         self.assertEquals(result, 'I')
 
-    #kcasckas
+    def test_decimal_to_roman_2(self):
+        result = decimal_to_roman(2)
+        self.assertEquals(result, 'II')
+
+    def test_decimal_to_roman_3(self):
+        result = decimal_to_roman(3)
+        self.assertEquals(result, 'III')
+
+    def test_decimal_to_roman_4(self):
+        result = decimal_to_roman(4)
+        self.assertEquals(result, 'IV')
+
+    def test_decimal_to_roman_5(self):
+        result = decimal_to_roman(5)
+        self.assertEquals(result, 'V')
+    def test_decimal_to_roman_6(self):
+        result = decimal_to_roman(6)
+        self.assertEquals(result, 'VI')
+    def test_decimal_to_roman_7(self):
+        result = decimal_to_roman(7)
+        self.assertEquals(result, 'VII')
+    def test_decimal_to_roman_8(self):
+        result = decimal_to_roman(8)
+        self.assertEquals(result, 'VIII')
+    def test_decimal_to_roman_9(self):
+        result = decimal_to_roman(9)
+        self.assertEquals(result, 'IX')
+    def test_decimal_to_roman_10(self):
+        result = decimal_to_roman(10)
+        self.assertEquals(result, 'X')
+
+    def test_decimal_to_roman_11(self):
+        result = decimal_to_roman(11)
+        self.assertEquals(result, 'XI')
+
+    def test_decimal_to_roman_12(self):
+        result = decimal_to_roman(12)
+        self.assertEquals(result, 'XII')
+
+    def test_decimal_to_roman_13(self):
+        result = decimal_to_roman(13)
+        self.assertEquals(result, 'XIII')
+
+    def test_decimal_to_roman_14(self):
+        result = decimal_to_roman(14)
+        self.assertEquals(result, 'XIV')
+
+    def test_decimal_to_roman_15(self):
+        result = decimal_to_roman(15)
+        self.assertEquals(result, 'XV')
+
+    def test_decimal_to_roman_16(self):
+        result = decimal_to_roman(16)
+        self.assertEquals(result, 'XVI')
+
+    def test_decimal_to_roman_17(self):
+        result = decimal_to_roman(17)
+        self.assertEquals(result, 'XVII')
+
+    def test_decimal_to_roman_18(self):
+        result = decimal_to_roman(18)
+        self.assertEquals(result, 'XVIII')
+
+    def test_decimal_to_roman_19(self):
+        result = decimal_to_roman(19)
+        self.assertEquals(result, 'XIX')
+
+    def test_decimal_to_roman_20(self):
+        result = decimal_to_roman(20)
+        self.assertEquals(result, 'XX')
+
+
+
+
 if __name__ == "__main__":
     unittest.main()
