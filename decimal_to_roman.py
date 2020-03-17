ea260e355733e6f46acd1ce83707cde9a9e1a496
Federico Acosta <facosta@eventbrite.com>
2020-03-17 15:45:56 -0300

    Code not working

--- decimal_to_roman.py
+++ decimal_to_roman.py
@@ -1,13 +1,30 @@
-response = ''
 
-def decimal_to_roman(number):
-	if (number > 10):
-		response += 'X'
-		decimal_to_roman(number - 10)
-	if (number > 5):
+class DecimalToRoman():
 
-	if number <= 0:
-		return ''
+	def __init__(self):
+		self.response = ""
+
+
+	def decimal_to_roman(self, number):
+		if (number > 10):
+			self.response += 'X'
+			return self.decimal_to_roman(number - 10)
+		elif (number > 5):
+			if number == 9:
+				self.response += 'IX'
+				return self.decimal_to_roman(number - 9)
+			else:
+				self.response += 'V'
+				return self.decimal_to_roman(number - 5)
+		elif number >= 1:
+			if number == 4:
+				self.response += 'IV'
+				return self.decimal_to_roman(number - 4)
+			else:
+				self.response += "I"
+				return self.decimal_to_roman(number - 1)
+		elif number <= 0:
+			return self.response
 
 
 
--- test_decimal_to_roman.py
+++ test_decimal_to_roman.py
@@ -1,87 +1,89 @@
 import unittest
-
-from decimal_to_roman import decimal_to_roman
+from decimal_to_roman import DecimalToRoman
 
 
 class TestDecimalToRoman(unittest.TestCase):
 
+    def __init__(self):
+        self.objeto = DecimalToRoman()
+
     def test_decimal_to_roman_0(self):
-        result = decimal_to_roman(0)
+        result = self.objeto.decimal_to_roman(0)
         self.assertEquals(result, '')
 
     def test_decimal_to_roman_1(self):
-        result = decimal_to_roman(1)
+        result = self.objeto.decimal_to_roman(1)
         self.assertEquals(result, 'I')
 
     def test_decimal_to_roman_2(self):
-        result = decimal_to_roman(2)
+        result = self.objeto.decimal_to_roman(2)
         self.assertEquals(result, 'II')
 
     def test_decimal_to_roman_3(self):
-        result = decimal_to_roman(3)
+        result = self.objeto.decimal_to_roman(3)
         self.assertEquals(result, 'III')
 
     def test_decimal_to_roman_4(self):
-        result = decimal_to_roman(4)
+        result = self.objeto.decimal_to_roman(4)
         self.assertEquals(result, 'IV')
 
     def test_decimal_to_roman_5(self):
-        result = decimal_to_roman(5)
+        result = self.objeto.decimal_to_roman(5)
         self.assertEquals(result, 'V')
     def test_decimal_to_roman_6(self):
-        result = decimal_to_roman(6)
+        result = self.objeto.decimal_to_roman(6)
         self.assertEquals(result, 'VI')
     def test_decimal_to_roman_7(self):
-        result = decimal_to_roman(7)
+        result = self.objeto.decimal_to_roman(7)
         self.assertEquals(result, 'VII')
     def test_decimal_to_roman_8(self):
-        result = decimal_to_roman(8)
+        result = self.objeto.decimal_to_roman(8)
         self.assertEquals(result, 'VIII')
     def test_decimal_to_roman_9(self):
-        result = decimal_to_roman(9)
+        result = self.objeto.decimal_to_roman(9)
         self.assertEquals(result, 'IX')
     def test_decimal_to_roman_10(self):
-        result = decimal_to_roman(10)
+        result = self.objeto.decimal_to_roman(10)
         self.assertEquals(result, 'X')
 
     def test_decimal_to_roman_11(self):
-        result = decimal_to_roman(11)
+        result = self.objeto.decimal_to_roman(11)
         self.assertEquals(result, 'XI')
 
     def test_decimal_to_roman_12(self):
-        result = decimal_to_roman(12)
+        result = self.objeto.decimal_to_roman(12)
         self.assertEquals(result, 'XII')
 
     def test_decimal_to_roman_13(self):
-        result = decimal_to_roman(13)
+        result = self.objeto.decimal_to_roman(13)
         self.assertEquals(result, 'XIII')
 
     def test_decimal_to_roman_14(self):
-        result = decimal_to_roman(14)
+        result = self.objeto.decimal_to_roman(14)
         self.assertEquals(result, 'XIV')
 
     def test_decimal_to_roman_15(self):
-        result = decimal_to_roman(15)
+        result = self.objeto.decimal_to_roman(15)
         self.assertEquals(result, 'XV')
 
     def test_decimal_to_roman_16(self):
-        result = decimal_to_roman(16)
+        result = self.objeto.decimal_to_roman(16)
         self.assertEquals(result, 'XVI')
 
     def test_decimal_to_roman_17(self):
-        result = decimal_to_roman(17)
+        result = self.objeto.decimal_to_roman(17)
         self.assertEquals(result, 'XVII')
 
     def test_decimal_to_roman_18(self):
-        result = decimal_to_roman(18)
+        result = self.objeto.decimal_to_roman(18)
         self.assertEquals(result, 'XVIII')
 
     def test_decimal_to_roman_19(self):
-        result = decimal_to_roman(19)
+        result = self.objeto.decimal_to_roman(19)
         self.assertEquals(result, 'XIX')
 
     def test_decimal_to_roman_20(self):
-        result = decimal_to_roman(20)
+        result = self.objeto.decimal_to_roman(20)
         self.assertEquals(result, 'XX')
 
 
