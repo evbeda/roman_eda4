8e1a3de47d779f0699cd505fdcb2496c0e3dd9a8
Axel Fernandez <axelfernandez@eventbrite.com>
2020-03-17 15:47:29 -0300

    no funciona maaaas

--- roman_to_decimal.py
+++ roman_to_decimal.py
@@ -1,16 +1,19 @@
+
 def roman_to_decimal(roman):
     result=0
     romano_valor = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
 
-    primer_valor = romano_valor[roman[0]]
+    result = romano_valor[roman[0]]
 
-        
+    import pdb;
+    pdb.set_trace()
 
     for i in range(1, len(roman)):
         if romano_valor[roman[i]] < romano_valor[roman[i-1]]:
-            result -= romano_valor[roman[i]]
+            result -= romano_valor[roman[i-1]]
         else:
-            result += romano_valor[roman[i]]
+            result += romano_valor[roman[i-1]]
+
 
 
     return result
